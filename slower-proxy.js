d47ddb88e0cb515cc247d1d9926f9129dc7485a1
ooxi <violetland@mail.ru>
2013-11-05 14:24:48 +0100

    slower-proxy initial development...

--- slower-proxy.js
+++ slower-proxy.js
@@ -5,9 +5,15 @@ var proxy = require('http-proxy');
 
 proxy.createServer(function(req, res, proxy) {
 	
+	var host = req.headers.host;
+	var colon = host.lastIndexOf(':');
+	
 	var options = {
-		target: req.headers.host
+		host: -1 === colon ? host : host.substr(0, colon),
+		port: -1 === colon ? 80 : parseInt(host.substr(colon))
 	};
+	console.log(options);
+	
 	proxy.proxyRequest(req, res, options);
 	
 }).listen(8080);
